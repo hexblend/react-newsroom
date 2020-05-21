28292f7a66f67fac63154ee1c4a1a542ce31e0b7
vladbbr <vladbbr1@gmail.com>
2020-05-21 23:47:15 +0100

    Sass setup

--- src/js/redux/actions/index.js
+++ src/js/redux/actions/index.js
@@ -1,12 +1,17 @@
-import { ADD_ARTICLE, DATA_LOADED } from '../constants/action-types';
+import {
+	ADD_ARTICLE,
+	DATA_LOADED,
+} from '../constants/action-types';
 
 export function addArticle(payload) {
 	return { type: ADD_ARTICLE, payload };
 }
 
 export function getData() {
-	return function(dispatch) {
-		return fetch('https://jsonplaceholder.typicode.com/posts')
+	return function (dispatch) {
+		return fetch(
+			'https://jsonplaceholder.typicode.com/posts'
+		)
 			.then((response) => response.json())
 			.then((payload) => {
 				dispatch({ type: DATA_LOADED, payload });
