be6141c1eebdb0f14799c3839f1e36abb5847fbe
vladbbr <vladbbr1@gmail.com>
2020-05-22 21:10:06 +0100

    Sidebar done

--- src/js/layout/Sidebar.js
+++ src/js/layout/Sidebar.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
-import { membersRef } from '../services/firebase';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect } from 'react-redux-firebase';
 
 function Sidebar() {
-	useEffect(() => {
-		// const members = membersRef.get();
-		// console.log(members);
-	}, []);
+	useFirestoreConnect([{ collection: 'users' }]);
+	const users = useSelector((state) => state.firestore.ordered.users);
 
 	return (
-		<div>
-			<h1>Sidebar</h1>
+		<div className="Sidebar">
+			{users &&
+				users.map((user) => (
+					<div className="User" key={user.email}>
+						<div
+							className="User__avatar"
+							style={{ backgroundImage: `url(${user.avatarUrl})` }}
+						></div>
+						<p className="User__fullName">{user.displayName}</p>
+					</div>
+				))}
 		</div>
 	);
 }
