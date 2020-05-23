6c439baf60d326182239276b5896aaa26ca148ed
vladbbr <vladbbr1@gmail.com>
2020-05-23 16:58:03 +0100

    Notification for news deleted

--- src/js/components/SingleNews.js
+++ src/js/components/SingleNews.js
@@ -5,9 +5,14 @@ import { useFirestore } from 'react-redux-firebase';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { setAlert } from '../redux/actions/MainActions';
+import { useDispatch } from 'react-redux';
 
 function SingleNews(props) {
 	const { news } = props;
+
+	const dispatch = useDispatch();
+
 	const users = useSelector((state) => state.firestore.ordered.users);
 	const author = users && users.filter((user) => user.id === news.author)[0];
 	const authUser = useSelector((state) => state.firebase.auth);
@@ -15,7 +20,16 @@ function SingleNews(props) {
 	const firestore = useFirestore();
 
 	const handleDeleteNews = () => {
-		firestore.collection('news').doc(news.id).delete();
+		firestore
+			.collection('news')
+			.doc(news.id)
+			.delete()
+			.then(() => {
+				dispatch(setAlert('News deleted.'));
+				setTimeout(() => {
+					dispatch(setAlert(''));
+				}, 1500);
+			});
 	};
 
 	return (
