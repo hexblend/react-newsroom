1af664c3f7785229d06318f92fcd30afda6da3bf
vladbbr <vladbbr1@gmail.com>
2020-05-23 08:28:24 +0100

    Error checking added to the input

--- src/js/components/AddNewsBar.js
+++ src/js/components/AddNewsBar.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
 import { connect } from 'react-redux';
 import { raiseInput } from '../redux/actions/MainActions';
+import { useFirestore } from 'react-redux-firebase';
 
 function AddNewsBar(props) {
-	const { raisedInput, setRaisedInput } = props;
+	const { auth, raisedInput, setRaisedInput } = props;
+
 	const [news, setNews] = useState('');
+	const [error, setError] = useState('');
+
+	const firestore = useFirestore();
+
+	const handleSubmit = () => {
+		if (news === '') {
+			setError("You can't submit an empty news.");
+		} else {
+			setError('');
+			setNews('');
+			setRaisedInput(false);
+			const newNews = {
+				news,
+				author: auth.uid,
+			};
+			firestore.collection('news').add(newNews);
+		}
+	};
 
 	return (
 		<div className={`AddNewsBar ${raisedInput && 'raised'}`}>
+			{error !== '' && <p className="AddNewsBar__error">{error}</p>}
 			<textarea
 				type="text"
 				className="AddNewsBar__input"
@@ -22,7 +44,7 @@ function AddNewsBar(props) {
 			<FontAwesomeIcon
 				icon={faPaperPlane}
 				className="AddNewsBar__icon"
-				onClick={() => setRaisedInput(false)}
+				onClick={() => handleSubmit()}
 			/>
 		</div>
 	);
@@ -30,6 +52,7 @@ function AddNewsBar(props) {
 
 const mapStateToProps = (state) => ({
 	raisedInput: state.MainReducer.raisedInput,
+	auth: state.firebase.auth,
 });
 
 const mapDispatchToProps = (dispatch) => ({
