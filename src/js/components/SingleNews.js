fcaea7477706689d12816529d763daa40cc0e5b4
vladbbr <vladbbr1@gmail.com>
2020-05-23 12:58:06 +0100

    Delete button jsx + styling

--- src/js/components/SingleNews.js
+++ src/js/components/SingleNews.js
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 function SingleNews(props) {
 	const { news } = props;
 	const users = useSelector((state) => state.firestore.ordered.users);
 	const author = users && users.filter((user) => user.id === news.author)[0];
+	const authUser = useSelector((state) => state.firebase.auth);
 
 	return (
 		<div className="SingleNews">
@@ -14,6 +18,11 @@ function SingleNews(props) {
 				<p className="SingleNews__footer--pin"></p>
 				<p className="SingleNews__footer--author">{author.displayName}</p>
 			</div>
+			{authUser.uid === author.id && (
+				<div className="SingleNews__deleteWrapper">
+					<FontAwesomeIcon icon={faTimes} className="SingleNews__delete" />
+				</div>
+			)}
 		</div>
 	);
 }
